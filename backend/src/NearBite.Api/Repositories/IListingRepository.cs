d0cc994760d6c267b84217079471a8dc18a6a4ea
Subhash <hvsjeewantha4@gmail.com>
2026-08-03 12:16:17 +0530

    feat: add IListingRepository interface and InMemoryListingRepository

---
+++ backend/src/NearBite.Api/Repositories/IListingRepository.cs
@@ -0,0 +1,12 @@
+﻿using NearBite.Api.Domain;
+
+namespace NearBite.Api.Repositories;
+
+public interface IListingRepository
+{
+    IEnumerable<Listing> GetAll();
+    Listing? GetById(int id);
+    void Add(Listing listing);
+    void Update(Listing listing);
+    void Delete(int id);
+}
